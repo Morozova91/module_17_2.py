246ec374ebf513993ad359eabc57c6ae6c7ab9c5
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-10-22 10:16:10 +0300

    Update db.py

--- db.py
+++ db.py
@@ -12,3 +12,9 @@ SessionLocal = sessionmaker(bind=engine) # и локальная сессия
 class Base(DeclarativeBase): # Создайте базовый класс Base для других моделей, наследуясь от DeclarativeBase
     pass
 
+
+class User(Base):
+    __tablename__="users"
+    id=Column(Integer,primary_key=True,index=True)
+    username=Column(String)
+    age=Column(Integer)
